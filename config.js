d7fdf83b967e7fa3763fea7daa3a7ae060ddb373
phutran15052003 <92377315+phutran15052003@users.noreply.github.com>
2021-10-23 09:38:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = '';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng sinh nhật cô gái của anh';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc cô gái của anh sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cô gái trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng sinh nhật người anh em';
+const titleCard = 'Tặng Gia Vương';
+const contentCard = 'Chúc người anh em sinh nhật tràn ngập niềm vui. Mong điều đẹp nhất sẽ đến với người anh em trong hôm nay và cả những ngày sau. Thành công trên con đường mình chọn';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
